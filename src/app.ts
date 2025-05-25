8dd54ca55bc767c07ee6652664e3dad6a8f311f3
amitsengupta <amitsengupta332@gmail.com>
2025-05-26 02:03:01 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
+const app: Application = express();
+
+
+//parser
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+
+const getAController = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', getAController);
+
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello World!");
+// });
+
+export default app;
