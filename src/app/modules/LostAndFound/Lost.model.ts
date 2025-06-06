8b9b515858a0ae8c5e3a6a47ffd47a445213b486
amitsengupta <amitsengupta332@gmail.com>
2025-06-06 23:00:32 +0600

    done lost model

--- src/app/modules/LostAndFound/Lost.model.ts
+++ src/app/modules/LostAndFound/Lost.model.ts
@@ -1,21 +1,23 @@
-// src/modules/LostAndFound/Lost.model.ts
-import { Schema, model } from 'mongoose';
-import { TLostItem } from './Lost.interface';
+import { model, Schema } from "mongoose";
+import { TLostItem } from "./Lost.interface";
+
 
 const LostItemSchema = new Schema<TLostItem>({
-  title: { type: String, required: true },
-  description: String,
-  category: { 
-    type: String, 
-    enum: ['ID Card', 'Phone', 'Laptop', 'Other'], 
-    required: true 
-  },
-  status: { 
-    type: String, 
-    enum: ['lost', 'found'], 
-    default: 'lost' 
-  },
-  image: String
-}, { timestamps: true }); // Adds `createdAt` and `updatedAt` automatically
+
+    title: { type: String, required: true },
+    description: String,
+    category: {
+        type: String,
+        enum: ['ID Card', 'Phone', 'Laptop', 'Other'],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['lost', 'found'],
+        default: 'lost'
+    },
+    image: String
+}, { timestamps: true });
+
 
 export const LostItem = model<TLostItem>('LostItem', LostItemSchema);
