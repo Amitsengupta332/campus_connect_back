f6925810978f3ac867d9950db7b6ecd9b7d51c53
amitsengupta <amitsengupta332@gmail.com>
2025-06-06 23:08:00 +0600

    in service create lost item

---
+++ src/app/modules/LostAndFound/Lost.service.ts
@@ -0,0 +1,27 @@
+import { TLostItem } from "./Lost.interface"
+import { LostItem } from "./Lost.model"
+
+const creatLostItem = async (lostData: TLostItem) => {
+    const result = await LostItem.create(lostData);
+    return result;
+}
+
+// const getLostItem = async () => {
+//     const result = await LostItem.find();
+//     return result;
+// }
+// const getLostItemById = async (id: string) => {
+//     const result = await LostItem.findById(id);
+//     return result;
+// }
+
+// const updateLostItem = async() =>{
+
+}
+
+export const LostService = {
+    creatLostItem,
+    // getLostItem,
+    // getLostItemById,
+    // updateLostItem
+}
